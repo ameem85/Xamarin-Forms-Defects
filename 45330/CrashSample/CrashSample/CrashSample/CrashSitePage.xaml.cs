88e83d8a998749390234a23ffc8bf812a6c1f1bd
Zafar Ameem <zafar.ameem@gmail.com>
2017-08-29 13:36:34 -0400

    initial checkin
    initial checkin

---
+++ 45330/CrashSample/CrashSample/CrashSample/CrashSitePage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CrashSample
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CrashSitePage : ContentPage
+    {
+        private ObservableCollection<ListViewItemType> _items;
+        public ObservableCollection<ListViewItemType> Items
+        {
+            get => _items;
+            set
+            {
+                if (value == _items)
+                {
+                    return;
+                }
+                _items = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public CrashSitePage()
+        {
+            InitializeComponent();
+            Items = ListViewItemsProvider.Items;
+            //ListView.SetBinding(ListView.ItemsSourceProperty, new Binding(".", source: ListViewItemsProvider.Items));
+        }
+
+        private void ListView_OnItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var item = e.Item as ListViewItemType;
+            item.IsSelected = !item.IsSelected;
+        }
+
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            //Reset the value to null. 
+            //since the lifetime of ItemsSource is greater than the page's listview, 
+            //resetting items to null to avoid triggering UI changes when any property changes occur on the ListViewItemType
+            Items = null;
+            Navigation.PopAsync();
+        }
+    }
+
+    public class ListViewItemsProvider
+    {
+        public static ObservableCollection<ListViewItemType> Items { get; } = new ObservableCollection<ListViewItemType>(new[]
+        {
+            new ListViewItemType {ItemName = "Zero"},
+            new ListViewItemType {ItemName = "One"},
+            new ListViewItemType {ItemName = "Two"},
+        });
+    }
+}
---
+++ 45330/CrashSample/CrashSample/CrashSample/ListViewItemType.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CrashSample
+{
+    public class ListViewItemType : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private bool _isSelected;
+        public bool IsSelected
+        {
+            get => _isSelected;
+            set
+            {
+                if (value == _isSelected)
+                {
+                    return;
+                }
+
+                _isSelected = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _itemName;
+        public string ItemName
+        {
+            get => _itemName;
+            set
+            {
+                if (value == _itemName)
+                {
+                    return;
+                }
+                _itemName = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
